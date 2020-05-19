e071ecf5d4d1f6f6dabb51a96fa2d10bf560624f
Ollejj <ollejohansson1996@gmail.com>
2020-05-19 11:49:59 +0200

    first

---
+++ src/test/java/team/alex/Grupp1/Grupp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package team.alex.Grupp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
