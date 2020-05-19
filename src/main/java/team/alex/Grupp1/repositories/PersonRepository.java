d058fd0652beaf69cde97aca5cac18ff8bc9a420
Ollejj <ollejohansson1996@gmail.com>
2020-05-19 14:29:24 +0200

    added h2 properties in application.properties

--- src/main/java/team/alex/Grupp1/repositories/PersonRepository.java
+++ src/main/java/team/alex/Grupp1/repositories/PersonRepository.java
@@ -1,7 +1,6 @@
 package team.alex.Grupp1.repositories;
 
-import javax.persistence.Entity;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, String> {}
+
