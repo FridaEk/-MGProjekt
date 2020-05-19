6ed57e740f4526d8e4ec05967e7be5fad69ca85c
Ollejj <ollejohansson1996@gmail.com>
2020-05-19 14:48:08 +0200

    added import in PersonRepository.java to Person.java

--- src/main/java/team/alex/Grupp1/repositories/PersonRepository.java
+++ src/main/java/team/alex/Grupp1/repositories/PersonRepository.java
@@ -2,5 +2,7 @@ package team.alex.Grupp1.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import team.alex.Grupp1.entity.Person;
+
 public interface PersonRepository extends CrudRepository<Person, String> {}
 
