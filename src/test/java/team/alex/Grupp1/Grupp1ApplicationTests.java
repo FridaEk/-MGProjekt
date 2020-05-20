1ad0e10491b22d3a8bf44925e622a76b0605ce4c
Ollejj <ollejohansson1996@gmail.com>
2020-05-20 12:45:52 +0200

    added testIfRowExists

--- src/test/java/team/alex/Grupp1/Grupp1ApplicationTests.java
+++ src/test/java/team/alex/Grupp1/Grupp1ApplicationTests.java
@@ -1,13 +1,34 @@
 package team.alex.Grupp1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import team.alex.Grupp1.repositories.PersonRepository;
 
 @SpringBootTest
 class Grupp1ApplicationTests {
 
+	private static long ssn;
+	
+	@Autowired
+	PersonRepository personRepo;
+	
+	@BeforeAll
+	public static void beforeAllTests() {
+		ssn = 101010;
+	}
+	
 	@Test
-	void contextLoads() {
+	@DisplayName("Test4")
+	public void testIfRowExists() {
+		assertFalse(personRepo.existsById(ssn));
 	}
 
+	
 }
