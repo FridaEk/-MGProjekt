c68f91cca6877f731776b997cb9a71dc12ffa7f5
idontreallywolf <nedim.kanat1999@gmail.com>
2020-05-20 11:24:22 +0200

    override default findAll and findById repository methods

--- src/main/java/team/alex/Grupp1/repositories/PersonRepository.java
+++ src/main/java/team/alex/Grupp1/repositories/PersonRepository.java
@@ -1,8 +1,20 @@
 package team.alex.Grupp1.repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import team.alex.Grupp1.entity.Person;
 
-public interface PersonRepository extends CrudRepository<Person, String> {}
+public interface PersonRepository extends CrudRepository<Person, Long> {
+	
+	// Overrides default findAll to return 
+	// an ArrayList of Person entities
+	ArrayList<Person> findAll();
+	
+	// Overrides default findById to return 
+	// a Person entity instead of Optional<Object>
+	Person findById(long ssn);
+	
+}
 
