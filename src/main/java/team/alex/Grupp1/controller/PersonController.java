6d24b4ff62f59c03c45134a753384b8c392a52c4
idontreallywolf <nedim.kanat1999@gmail.com>
2020-05-28 11:54:49 +0200

    add documentation to personController & resonseObject

--- src/main/java/team/alex/Grupp1/controller/PersonController.java
+++ src/main/java/team/alex/Grupp1/controller/PersonController.java
@@ -19,6 +19,12 @@ public class PersonController {
 	@Autowired
 	PersonRepository personRepo;
 
+    /**
+    *   Creates a person with given data if provided ssn is not already present in the database.
+    *
+    *   @param personData - Person-entity model
+    *   @return an error message if an entity with given ssn exists otherwise a success message.
+    */
     @PostMapping("/createPerson")
     public ResponseObject createPerson(@RequestBody Person personData) {
     	
@@ -40,6 +46,14 @@ public class PersonController {
         return new ResponseObject("Created: " + person.toString());
     }
 
+    /**
+        Attempts to delete a Person entry in the database given ssn parameter.
+
+        @param ssn - social security number
+        @return a string ' "Deleted: ssn | lastname | firstname | getAge | getGender | getCity | getCountry " ' if an entity with given SSN exists.<br>
+        Otherwise an error message indicating that such entity does not exist:<br>
+        <i>Error: Entity with SSN '...' doesn't exist.</i>
+    */
     @DeleteMapping("/deletePerson/{ssn}")
     public ResponseObject deletePerson(@PathVariable long ssn) {
     	
@@ -54,8 +68,10 @@ public class PersonController {
     }
 
 
-    /*
-    * Retrieves all existing entities(Person) and returns them as an ArrayList<Person>
+    /**
+    *   Retrieves all existing entities(Person) and returns them as an ArrayList<Person>
+    *
+    *   @return a stringifed version of a list containing info on each entity
     */
     @GetMapping("/fetchAll")
     public String getAll() {
@@ -73,16 +89,32 @@ public class PersonController {
         return fetchedData.toString();
     }
 
-    /*
-    * Retrieves a Person entity by given SSN id
+    /**
+    *   Retrieves a Person entity by given SSN
+    *
+    *   @return a ResponseObject containing a stringified version of the retrieved entity if exists, otherwise an error message.
     */
     @GetMapping("/getPersonBySSN/{ssn}")
     public String getPersonBySSN(@PathVariable long ssn) {
-        return personRepo.findById(ssn).toString();
+
+        if(personRepo.existsById(ssn) == true)
+            return personRepo.findById(ssn).toString();
+
+        return "a person with "+ssn+" doesn't exist.";
     }
 
+    /**
+    *   Modifies an existing Person-entity.
+    *
+    *   @param personData - Person-entity model
+    *   @return a string containing information on the modified person-entity,<br>
+    *   otherwise an error message if the entity doesn't exist.
+    */
     @PostMapping("/changePerson")
     public String changePerson(@RequestBody Person personData) {
+
+        if(personRepo.existsById(personData.getSsN()) == false)
+            return "such entity does not exist";
 
     	Person oldPerson = personRepo.findById(personData.getSsN());
 
--- src/main/java/team/alex/Grupp1/controller/ResponseObject.java
+++ src/main/java/team/alex/Grupp1/controller/ResponseObject.java
@@ -1,16 +1,34 @@
 package team.alex.Grupp1.controller;
 
+/**
+*   ResponseObject class acts as a container<br>
+*   for response messages which are later converted<br>
+*   to JSON format through SpringBoot
+*/
 public class ResponseObject {
     private String response;
 
+    /**
+    *   Constructor
+    *
+    *   @param response - a string to be used as response message
+    */
     public ResponseObject(String response) {
         setResponse(response);
     }
 
+    /**
+    *   Sets response message.
+    *
+    *   @param response - a string to be used as response message
+    */
     public void setResponse(String response) {
         this.response = response;
     }
 
+    /**
+    *   @return response message
+    */
     public String getResponse() {
         return this.response;
     }
